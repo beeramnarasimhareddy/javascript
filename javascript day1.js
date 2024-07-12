360772df3919c8c0733749b63444c371a645bdf8
Beeram NarasimhaReddy <narasimhabeeram80@gmail.com>
2024-07-12 17:14:10 +0530

    Add files via upload

---
+++ javascript day1.js
@@ -0,0 +1,93 @@
+//daily task-1
+//Datatypes
+
+//string
+let str="narasimha"
+console.log(typeof str)
+console.log(str)
+
+//Number
+let num=30
+console.log(typeof num)
+console.log(num)
+
+//boolean
+let a=4
+let b=8
+let c= a==b
+console.log(typeof c)
+console.log(c)
+
+//null
+let d=null
+console.log(typeof d)
+console.log(d)
+
+//undefined
+let e;
+console.log(typeof e)
+console.log(e)
+
+
+//Mathematical operators
+
+
+let height=12;
+let width=10;
+
+//addition
+console.log("addition:",height+width)
+//subtraction
+console.log("subtraction:",height-width)
+//multiplication
+console.log("Multiplication:",height*width)
+//division
+console.log("division:",a/b)
+//modulus
+console.log("modulus:",height%width)
+
+
+//comparision operators
+//same datatypes
+//==
+console.log("height==width:",height==width)
+
+//===
+console.log("height===width:",height===width)
+
+//!=
+console.log("height!=width:",height!=width)
+//> greater than
+console.log("height>width",height>width)
+
+// < lessthan
+console.log("height<width:",height<width)
+
+//>= greater than equalto
+console.log("height>=width",height>=width)
+
+//<= lessthan equalto
+console.log("height<=width",height<=width)
+
+//different datatypes
+weight=80
+area='45'
+//==
+console.log("weight==area:",weight==area)
+
+//===
+console.log("weight===area:",weight===area)
+
+//!=
+console.log("weight!=area:",weight!=area)
+//> greater than
+console.log("weight>area:",weight>area)
+
+// < lessthan
+console.log("weight<ar>ea:",weight<area)
+
+//>= greater than equalto
+console.log("weight>=area:",weight>=area)
+
+//<= lessthan equalto
+console.log("weight<=area:",weight<=area)
